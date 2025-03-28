f11d61eb2790ea5ca3121aed1377a44919dc1d14
Riley Eaton <ryeaton@student.ubc.ca>
2025-03-28 11:03:56 -0700

    add evalai script for evaluating on challenge set

---
+++ submit_challenge.bat
@@ -0,0 +1,6 @@
+evalai set_token 
+
+evalai challenge 1952 phase 3904 submit --file C:\Users\Riley\Documents\UBC\GitHub\COSC519-Jersey-Number-Recognition\out\SoccerNetResults\challenge_final_results_64.json --public
+
+@REM Then, to get results, run:
+@REM evalai submission ID result
