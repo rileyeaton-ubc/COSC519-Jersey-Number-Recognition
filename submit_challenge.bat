5f758b5b54582e39c60a895ea50be10d1200013a
Riley Eaton <ryeaton@student.ubc.ca>
2025-04-04 00:55:29 -0700

    add scripts to run evalai submissions and demo pipeline

--- submit_challenge.bat
+++ submit_challenge.bat
@@ -1,6 +1,6 @@
-evalai set_token 
-
-evalai challenge 1952 phase 3904 submit --file C:\Users\Riley\Documents\UBC\GitHub\COSC519-Jersey-Number-Recognition\out\SoccerNetResults\challenge_final_results_64.json --public
+evalai challenge 1952 phase 3904 submit --file C:\Users\Riley\Documents\UBC\GitHub\COSC519-Jersey-Number-Recognition\out\SoccerNetResults\challenge_final_results_64.json --private
 
 @REM Then, to get results, run:
 @REM evalai submission ID result
+@REM Best CHALLENGE submission:
+@REM 502408
--- test_submission.bat
+++ test_submission.bat
@@ -1,6 +1,6 @@
-evalai set_token 
-
-evalai challenge 1952 phase 3903 submit --file C:\Users\Riley\Documents\UBC\GitHub\COSC519-Jersey-Number-Recognition\out\SoccerNetResults\test_final_results.json --private
+evalai challenge 1952 phase 3903 submit --file C:\Users\Riley\Documents\UBC\GitHub\COSC519-Jersey-Number-Recognition\out\SoccerNetResults\test_final_results_64.json --private
 
 @REM Then, to get results, run:
 @REM evalai submission ID result
+@REM Best TEST submission:
+@REM 503140
